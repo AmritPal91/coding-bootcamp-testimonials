30149053407e30b1df26950c60f90cd7a6d81058
Amritpal Singh <100977877+AmritPal91@users.noreply.github.com>
2023-11-09 00:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ let testimonial = document.getElementById("testimonial");
 let name2 = document.getElementById("name");
 let designation = document.getElementById("designation");
 
-let users = {
-  name: "Tanya Sinclair",
-  designation: "UX Engineer",
-  image: "images/image-tanya.jpg",
-  comment: " I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."
+// let users = {
+//   name: "Tanya Sinclair",
+//   designation: "UX Engineer",
+//   image: "images/image-tanya.jpg",
+//   comment: " I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."
 
-},
-  {
-    name: "John Tarkpor",
-    designation: "Junior Front-end Developer",
-    image: "images/image-john.jpg",
-    comment: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."
-}
+// },
+//   {
+//     name: "John Tarkpor",
+//     designation: "Junior Front-end Developer",
+//     image: "images/image-john.jpg",
+//     comment: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."
+// }
 
 // Previous button function
 prev.addEventListener("click", () => {
